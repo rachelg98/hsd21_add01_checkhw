e6b175519df48751d88ce5184938f25f5796bc97
rachelg98 <ganrachel98@gmail.com>
2021-06-18 02:42:11 +0900

    Update printf logs

--- src/fpga_api.cpp
+++ src/fpga_api.cpp
@@ -38,7 +38,7 @@ const float* __attribute__((optimize("O0"))) FPGA::run()
 {
     printf("running...\n");
     *api_ = 0x5555;
-    printf("check api: %x\n", &api_);
+    printf("check api: %d,\t%x\n", &api_, api_);
     while(*api_ == 0x5555);
     printf("api: %h\ndata: %f\n", &api_, &data_);
     return data_;    
